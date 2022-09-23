893851a0bd204d53ab978e9e972d27f1fe980562
Ross <komarostyslav@gmail.com>
2022-09-23 16:23:41 +0300

    corrections

--- groups/models.py
+++ groups/models.py
@@ -6,4 +6,3 @@ class Groups(models.Model):
     name_group = models.CharField(max_length=100, null=False, blank=False)
     data_start = models.DateField(default=date.today, null=False, blank=False)
     description = models.TextField()
-    
