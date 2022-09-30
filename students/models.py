4e97786474b65130c5802bbd9fb045165760d42f
Ross <komarostyslav@gmail.com>
2022-09-30 22:10:15 +0300

    hw15

--- students/models.py
+++ students/models.py
@@ -20,7 +20,7 @@ class Student(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
-    
+
     class Meta:
         db_table = 'student_table'
         verbose_name_plural = "Students"
